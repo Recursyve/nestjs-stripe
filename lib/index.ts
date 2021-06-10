92d8d8a4e87edcb0c32c5560d8126d5c2cad15e8
Etienne Lescarbeault <elescarbeault@recursyve.io>
2021-06-10 11:17:47 -0400

    Added subscriptions module

--- lib/index.ts
+++ lib/index.ts
@@ -3,3 +3,5 @@ export * from "./modules/customers";
 export * from "./modules/charges";
 export * from "./modules/refunds";
 export * from "./modules/prices";
+export * from "./modules/products";
+export * from "./modules/subscriptions";
