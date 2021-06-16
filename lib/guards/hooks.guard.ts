7956d0758aea91aad8942276197b34c888271bf3
Etienne Lescarbeault <elescarbeault@recursyve.io>
2021-06-16 09:53:16 -0400

    Added guard for hook validation

---
+++ lib/guards/hooks.guard.ts
@@ -0,0 +1,34 @@
+import {CanActivate, ExecutionContext, Injectable} from "@nestjs/common";
+import {Observable} from "rxjs";
+import {StripeConfigService} from "../modules/config/services/stripe-config.service";
+import {StripeWebHooksService} from "../modules/webhooks";
+
+@Injectable()
+export class HooksGuard implements CanActivate {
+
+    constructor(
+        private readonly stripeConfigService: StripeConfigService,
+        private readonly stripeWebHooksService: StripeWebHooksService
+    ) {
+    }
+
+    public canActivate(context: ExecutionContext): boolean  {
+        let canActivate: boolean;
+
+        const request = context.switchToHttp().getRequest();
+        const endpointSecret = this.stripeConfigService.global?.webhookEndpointSecret;
+
+        if(!endpointSecret) {
+            return false;
+        }
+
+        const signature = request.headers["stripe-signature"];
+        try {
+            canActivate = !!this.stripeWebHooksService.constructEvent(request.body, signature, endpointSecret);
+        } catch (error) {
+            canActivate = false;
+        }
+
+        return canActivate;
+    }
+}
--- lib/modules/config/models/stripe-config.model.ts
+++ lib/modules/config/models/stripe-config.model.ts
@@ -1,11 +1,13 @@
 export interface StripeConfigModel {
     clientSecret: string;
+    webhookEndpointSecret?: string;
 }
 
 export class StripeConfigModel {
     public clientSecret: string;
-
+    public webhookEndpointSecret?: string;
     constructor(config: Partial<StripeConfigModel>) {
         this.clientSecret = config.clientSecret ?? process.env.STRIPE_CLIENT_SECRET;
+        this.webhookEndpointSecret = config.webhookEndpointSecret ?? process.env.WEBHOOK_ENDPOINT_SECRET;
     }
 }
