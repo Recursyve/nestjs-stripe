cbcbeecfa6d570cf9d99225b96a354da35801d1b
Félix Dufresne <72946495+Duff97@users.noreply.github.com>
2024-11-29 14:30:00 -0500

    Subscription items module (#10)
    * subscription items module implementation
    
    * added new subscription-items module to exports

--- lib/index.ts
+++ lib/index.ts
@@ -11,3 +11,4 @@ export * from "./modules/invoices";
 export * from "./modules/payment-intents";
 export * from "./modules/webhooks";
 export * from "./modules/setup-intents";
+export * from "./modules/subscription-items";
